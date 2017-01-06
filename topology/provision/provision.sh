9b39cd9e773bb01d1560da4c5c20abe025b3967e
Fabien Franchini <fabien.franchini@protonmail.ch>
2017-01-06 13:45:41 +0100

    Mis à jours de la construction des images docker

--- topology/provision/provision.sh
+++ topology/provision/provision.sh
@@ -27,8 +27,10 @@ docker run -d --name web-app fhr/wordpress
 # Run the Varnish container
 echo "### Run the Varnish container"
 docker rm -f rproxy 2>/dev/null || true
-docker run -d -p 80:80 \
+docker run -d -p 80:80 -p 6802:6802 \
 --link web-app:backend-host \
 --volumes-from web-app \
 --env 'VCL_CONFIG=/data/config/varnish.vcl' \
---name rproxy million12/varnish
+--env 'VARNISH_ADMIN_LISTEN_PORT=6802' \
+--env 'VARNISHD_PARAMS=-p default_ttl=30' \
+--name rproxy fhr/varnish
