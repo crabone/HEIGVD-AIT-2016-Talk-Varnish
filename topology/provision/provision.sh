dbecfb2529ab095e1f6d58730f79e73541cdf100
Fabien Franchini <fabien.franchini@protonmail.ch>
2017-01-06 09:36:17 +0100

    Ajout d'utilitaires dans la topologie

--- topology/provision/provision.sh
+++ topology/provision/provision.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Update the package index
+# Update the package index & install utilities
 echo "### Update the package index"
-apt-get update
+apt-get update && apt-get install httpie -y
 
 # Install Docker
 echo "### Install Docker"
@@ -20,11 +20,15 @@ cd /vagrant/images/wordpress
 docker build -t fhr/wordpress .
 
 # Run the web application container
-echo "Run the web application container"
+echo "### Run the web application container"
 docker rm -f web-app 2>/dev/null || true
 docker run -d --name web-app fhr/wordpress
 
 # Run the Varnish container
 echo "### Run the Varnish container"
 docker rm -f rproxy 2>/dev/null || true
-docker run -d -p 80:80 --link web-app:backend-host --volumes-from web-app --env 'VCL_CONFIG=/data/config/varnish.vcl' --name rproxy million12/varnish
+docker run -d -p 80:80 \
+--link web-app:backend-host \
+--volumes-from web-app \
+--env 'VCL_CONFIG=/data/config/varnish.vcl' \
+--name rproxy million12/varnish
